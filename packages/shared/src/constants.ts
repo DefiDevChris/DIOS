2fb0dad551f738fe027866d53b9c6f2b919eaa35
BigC <204614606+DefiDevChris@users.noreply.github.com>
2026-03-16 22:00:55 -0500

    fix: replace restricted gmail.modify scope with gmail.send to fix OAuth blocked error
    gmail.modify is a restricted scope that Google blocks entirely for unverified
    apps. gmail.send is a sensitive scope that allows users to proceed with a warning.

--- packages/shared/src/constants.ts
+++ packages/shared/src/constants.ts
@@ -15,7 +15,7 @@ export const FIRESTORE_COLLECTIONS = {
 export const OAUTH_SCOPES = [
   'https://www.googleapis.com/auth/drive.file',
   'https://www.googleapis.com/auth/calendar.events',
-  'https://www.googleapis.com/auth/gmail.modify',
+  'https://www.googleapis.com/auth/gmail.send',
   'https://www.googleapis.com/auth/spreadsheets',
   'https://www.googleapis.com/auth/userinfo.profile',
   'https://www.googleapis.com/auth/userinfo.email',
